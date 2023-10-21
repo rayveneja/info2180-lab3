2a9c1c276955acaa52ac8bff5254c7b615fc10bf
rayveneja <rayvenejenningsallen@gmail.com>
2023-10-21 13:22:39 -0400

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sq = document.querySelectorAll('#board > div');
+    let currentPlayer = 'X';
+    
     sq.forEach(function (square) {
         square.classList.add('square');
-    });
+        // question 2
 
+        square.addEventListener('click', function(){
+            if(!square.classList.contains('X') && !square.classList.contains('O')){
+                square.classList.add(currentPlayer);
+                square.textContent = currentPlayer;
+                if(currentPlayer === 'X'){
+                    currentPlayer = 'O';
+                }else{
+                    currentPlayer = 'X';
+                }
+            }
+        });
+
+    });
 });
